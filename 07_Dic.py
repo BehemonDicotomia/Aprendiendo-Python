497a410410d5b100cb2c6e662bc32d1e65da4b5c
BehemonDicotomia <Telemanco@proton.me>
2025-04-25 07:43:46 +0200

    More_examples
    Ejercicios_python

---
+++ 07_Dic.py
@@ -0,0 +1,63 @@
+#Diccionarios Ejercicios 
+
+
+my_dic = dict()
+print(my_dic)
+
+#Los diccionarios son estructuras de datos que almacenan pares de clave-valor.
+
+#Ejercicio 1 crear un diccionario con su clave y valor 
+
+dict = {'name': 'Marcos', 'age': 24, 'country': 'Mexico'}
+print(dict)
+
+#Ejercicio 2 accerder a un valor clave del diccionario 
+
+print(dict['name'])
+print(dict['Miserable'])
+print(dict.items())
+#Ejercicio 3 añadir una nueva clave job 
+
+dict['job'] = 'programador'
+print(dict)
+
+#Ejercicio 4 modificar el valor de una clave
+
+dict['age'] = 38
+print(dict)
+
+#Ejercicio 5 eliminar una clave del diccionario 
+
+del dict['country']
+print(dict)
+
+#Ejercicio 6 crear un diccionario con claves y valores que sean cuadrados
+cuadrados = {x: x**2 for x in range(1, 6)}
+print(cuadrados)
+
+#otra forma de hacerlo 
+
+cuadrados = {1:1, 2:4, 3:9, 4:16, 5:25}
+print(cuadrados)
+
+#Ejercicio 7 verificar si la clave age existe en el diccionario 
+
+my_dic = {'name': 'Brais', 'age': 37, 'country': 'Galicia'}
+
+print('age' in my_dic)
+
+#Ejercicio 8 imprimir solamente las claves del diccionario 
+
+llaves = my_dic.keys()
+print(llaves) 
+
+
+#Ejercicio 9 convertir las claves de un diccionario en una lista e imprimirla 
+llaves = list(my_dic.keys())
+print(llaves)
+
+#Ejercicio 10  crear un diccionario con el metodo fromkeys()
+
+claves = ['name', 'age', 'job']
+claves = dict.fromkeys(claves)
+print(claves.fromkeys(claves, 'Desconocido'))
---
+++ 10_functions.py
@@ -0,0 +1,89 @@
+# Funciones en Python
+
+#Ejercicio 1 crear una funcion 
+
+def personalized_greeting(name = 'Desconocido'):
+    print(f'Hola {name}')
+
+personalized_greeting('Carlos')
+personalized_greeting() # Imprime 'Hola Desconocido'
+
+#Ejercicio 2  crear una funcion multiply 
+
+def multiply(a, b):
+    return a * b
+
+result = multiply(5, 20)
+print(f'El resultado de la multiplicacion es: {result}')
+
+#Ejercicio 3  crear una funcion is_even 
+
+def is_even(number):
+    if number % 2 == 0:
+       return f'{number} es par {True}'
+    else: 
+       return f'{number} es impar {False}'
+
+print(is_even(6))
+
+# Ejercicio 4  crear una funcion convert_to_uppercase
+
+texto = input('Ingrese un texto: ')
+def convert_to_uppercase(texto):
+    return texto.upper()
+print(convert_to_uppercase(texto))
+
+#Ejercicio 5 crear una funcion arbitrary_sum 
+
+
+def arbitrary_sum(*args):
+    for i in args:
+        return sum(args)
+
+print(arbitrary_sum(1, 2, 35, 676, 7))
+
+
+#Ejercicio 6 crear una funcion generate_full_greeting
+
+def generate_full_greeting(name, surname):
+    return f'Hola {name} {surname}'
+
+print(generate_full_greeting('Carlos', 'Gonzales'))
+
+
+# Ejercicio 7 crear una funcion power y elevar un numero **2
+
+number1 = int(input('Ingrese un numero: '))
+number2 = int(input('Ingrese otro numero: '))
+
+def power(number1, number2):
+    return number1 ** number2
+
+print(f' EL numero {number1}, elevado a {number2} da como resultado:  {power(number1, number2)}')
+
+#Ejercicio 8 crear una funcion llamada calculate_averange
+
+num1 = int(input('Ingrese un numero: '))
+num2 = int(input('Ingrese un numero: '))
+num3 = int(input('Ingrese un numero: '))
+
+def calculate_average(*numbers):
+    return sum(numbers) / len(numbers)
+
+print(calculate_average(num1, num2, num3))
+
+#Ejercicio 9 crear una funcion llamada count_caraceters
+
+string  = input('Ingrese un texto: ')
+def count_characters(string):
+    return len(string)
+print(count_characters(string))
+
+
+#Ejercicio 10 crear una funcion llamada  display_message
+
+def display_message(*cadena):
+    for i in cadena: 
+        print(i.upper())
+    
+display_message('Hola', 'mundo', 'python')
